f79c9b72c5bc7a24322c913b16df1d686c05e7b9
Mohninyu <hninyuaungcu@gmail.com>
2018-06-05 22:54:48 +0630

    added ex5

---
+++ ex5.py
@@ -0,0 +1,20 @@
+my_name = 'Hnin Yu'
+my_age = 21
+my_height = 74
+my_weight = 100
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+
+print(f"Let's talk about {my_name}.")
+print(f"She's {my_height} inches tall.")
+print(f"She's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"She's got {my_eyes} and {my_hair} hair.")
+print(f"Her teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
