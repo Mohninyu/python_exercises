94c0fc98b429495e2af13f620b56efa5dd85698c
Mohninyu <hninyuaungcu@gmail.com>
2018-06-17 22:47:50 +0630

    added ex43

---
+++ ex43.py
@@ -0,0 +1,7 @@
+class Death(Scene):
+
+    quips = [ "You died. You kinda suck at this.", "Your Mom would be proud...if she were smarter.","Such a luser.", "I have a small puppy that's better at this.", "You're worse than your Dad's jokes." ]
+    def enter(self):
+        print(Death.quips[randint(0, len(self.quips)-1)])
+        exit(1)
+
