0511e9b75056e8e84ce9c47ead66433b52d35a51
Mohninyu <hninyuaungcu@gmail.com>
2018-06-18 12:00:36 +0630

    added setup

---
+++ setup.py
@@ -0,0 +1,6 @@
+try:
+    form setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = { 'description': 'My Project', 'author': 'My Name', 'url': 'URL to get it at.', 'download_url': 'Where to download it.', 'author_email': 'My email.', 'version': '0.1', 
