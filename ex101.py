6430ed398d57467ad8cde187638089e98f32855d
Mohninyu <hninyuaungcu@gmail.com>
2018-06-26 09:55:34 +0630

    added initial cat

--- ex101.py
+++ ex101.py
@@ -1,10 +1,10 @@
-tabby_rat = "\tI'm tabbed in."
-persion_rat = "I'm split\non a line."
-backslash_rat = "I'm \\ a \\ rat."
+tabby_cat = "\tI'm tabbed in."
+persion_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
 
-fat_rat = """ I'll do a list: \t* Rat food \t* Fishies \t* Catnip\n\t* Grass"""
+fat_cat = """ I'll do a list: \t* cat food \t* Fishies \t* Catnip\n\t* Grass"""
 
-print(tabby_rat)
-print(persion_rat)
-print(backslash_rat)
-print(fat_rat)
+print(tabby_cat)
+print(persion_cat)
+print(backslash_cat)
+print(fat_cat)
